f6503e031af581501445a6154ff176369c716328
YaegerMoon <yaeger@kakao.com>
2022-05-22 15:16:08 +0900

    chore: install yarn sdk for vscode

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,16 +3,23 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `gatsby-ts-template`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+
+      // __key: "pages",
     },
-    __key: "pages"
-  }]
+  ],
 };
 
 export default config;
